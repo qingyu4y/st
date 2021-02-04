bd0087725e8b01ac42e35b6e1fc20f3889677293
qingyu <qingyu_igardenia@163.com>
2021-02-04 18:46:41 +0800

    changed font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:Liberation Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:Source Code Pro:Liberation Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
